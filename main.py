10cb2d1a73cf83225f5e655934073516bfd3035f
Christopher Hatton <c.j.hatton@outlook.com>
2021-05-17 18:11:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -347,7 +347,7 @@ def f_main():
         submit = Button(popup,text = 'Submit',command = f_submit,bg = 'gray',fg = 'white')
         text_style_label.grid(row = 0,column = 0)
         text_style_entry.grid(row = 1,column = 0,padx = 5)
-        submit.grid(row = 2,column = 0,,pady = 3)
+        submit.grid(row = 2,column = 0,pady = 3)
         popup.bind('<Return>',f_submit_key)
 
     def f_change_text_style_key(event): #Changes font style of textbox
