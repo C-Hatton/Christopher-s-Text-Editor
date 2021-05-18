c8056affacc9c29e9d879823dc1e8cffa54eb230
Christopher Hatton <c.j.hatton@outlook.com>
2021-05-18 19:13:58 +0100

    Add files via upload

---
+++ standard.py
@@ -0,0 +1,11 @@
+def f_themes():
+    font_size = '15'
+    font_style = ''
+    font_type = 'Courier'
+    text_colour = 'black'
+    button_fg = 'white'
+    button_bg = 'gray'
+    heading_fg = 'black'
+    heading_bg = 'lightgray'
+    text_box_colour = 'white'
+    return font_size,font_style,font_type,text_colour,button_fg,button_bg,heading_fg,heading_bg,text_box_colour
