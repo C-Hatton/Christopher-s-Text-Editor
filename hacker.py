6457a00bb67ae97c99cd9504e972f82875b1927e
Christopher Hatton <c.j.hatton@outlook.com>
2021-05-18 19:15:20 +0100

    Add files via upload

---
+++ hacker.py
@@ -0,0 +1,11 @@
+def f_themes():
+    font_size = '20'
+    font_style = ''
+    font_type = 'Helvetica'
+    text_colour = 'lime'
+    button_fg = 'lime'
+    button_bg = 'black'
+    heading_fg = 'lime'
+    heading_bg = 'black'
+    text_box_colour = 'black'
+    return font_size,font_style,font_type,text_colour,button_fg,button_bg,heading_fg,heading_bg,text_box_colour
